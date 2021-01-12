aeaffbdfd71069c9ad4e55d7d807b8130743df93
lijyugao <b8989332@gmail.com>
2021-01-12 23:15:42 +0800

    add image list

--- InstagramClone/MainWindow.xaml.cs
+++ InstagramClone/MainWindow.xaml.cs
@@ -20,9 +20,38 @@ namespace InstagramClone
     /// </summary>
     public partial class MainWindow : Window
     {
+        public class MovieData
+        {
+            private BitmapImage _ImageData;
+            public BitmapImage ImageData
+            {
+                get { return this._ImageData; }
+                set { this._ImageData = value; }
+            }
+
+        }
+
         public MainWindow()
         {
             InitializeComponent();
+
+            this.TvBox.ItemsSource = new MovieData[]
+            {
+                new MovieData{ ImageData=LoadImage("Images\\beach-2089936_1920.jpg")},
+                new MovieData{ ImageData=LoadImage("Images\\gallery-3114279_1920.jpg")},
+                new MovieData{ ImageData=LoadImage("Images\\horses-1149974_1280.jpg")},
+                new MovieData{ ImageData=LoadImage("Images\\landscape-1802337_1920.jpg")},
+                new MovieData{ ImageData=LoadImage("Images\\parent-863085_1920.jpg")},
+                new MovieData{ ImageData=LoadImage("Images\\pedestrians-1209316_1920.jpg")}
+            };
+
+
+        }
+
+        // for this code image needs to be a project resource
+        private BitmapImage LoadImage(string filename)
+        {
+            return new BitmapImage(new Uri("pack://application:,,,/InstagramClone;component/" + filename));
         }
 
         private void Top_MouseDown(object sender, MouseButtonEventArgs e)
